1731428ccabf2fdbff54a9978c06bc59ff6012a6
Robert G <robzbakk@hotmail.co.uk>
2014-02-28 12:30:15 +0000

    no message

--- src/itemresearcher/userinterface/UserInterface.java
+++ src/itemresearcher/userinterface/UserInterface.java
@@ -45,7 +45,7 @@ public class UserInterface extends JPanel implements Runnable {
 		final JFrame frame = new JFrame();
 		frame.setTitle("ItemResearcher - A tool for researching items on the GrandExchange.");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(new UserInterface(), BorderLayout.CENTER);
+		frame.add(this, BorderLayout.CENTER);
 		frame.setSize(520, 205);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(frame.getOwner());
